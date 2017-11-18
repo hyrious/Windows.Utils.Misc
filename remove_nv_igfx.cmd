1d917f608cbaab7e56d72da48c3de13813a0cf6d
hyrious <hyrious@outlook.com>
2017-11-18 11:24:52 +0800

    remove display drivers' context menu

---
+++ remove_nv_igfx.cmd
@@ -0,0 +1,4 @@
+regsvr32 /s/u C:\Windows\system32\nv3dappshext.dll
+regsvr32 /s/u C:\Windows\system32\nvshext.dll
+regsvr32 /s/u "C:\Program Files\NVIDIA Corporation\Display\nvui.dll"
+regsvr32 /s/u C:\Windows\System32\igfxDTCM.dll
